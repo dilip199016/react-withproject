caa88837af507caad044172d7c278b0f20bac302
Dilip-Love-Code <vddilipvarma@gmail.com>
2023-10-12 05:40:28 +0530

    usestate

--- src/App.js
+++ src/App.js
@@ -1,55 +1,116 @@
-import {Buttton,Summary} from "./Component/button";
+// import {Buttton,Summary} from "./Component/button";
 import "./App.css"
-
-// import Timer from"./timer";
-// import {Click,Checker,My} from "./Component/input"; //NAMED IMPORT
-// import Checkbox from "./Component/checkbox";
-// import {JsxExample,JsxExample2,MultiElement} from "./Example.jsx"
- import  UseState  from "./Component/userstate.jsx";
 import { useState } from "react";
-// import User from "./user"
- //  import Orglist from "./constant/constant,js"
-// React App component
-function App() { //functional component
-  const[theme,setTheme]=useState("light")
+import Myhook from "./usestate2.jsx"
+//  import Timer from"./timer";
+//  import {Click,Checker,My} from "./Component/input"; //NAMED IMPORT
+//  import Checkbox from "./Component/checkbox";
+//  import {JsxExample,JsxExample2,MultiElement} from "./Example.jsx"
+//  import  UseState  from "./Component/userstate.jsx";
+// import { Buttton,Summary } from "./Component/button";
+ import User from "./user"
+  
+function App() { 
+   const[theme,setTheme]=useState("light")
   
   
-  const toggleTheme=()=>{
+   const toggleTheme=()=>{
     if (theme === "light") {
-      setTheme("dark");
-    } else if (theme === "dark") {
+        setTheme("dark");
+     } else if (theme === "dark") {
       setTheme("red");
     } else {
       setTheme("light");
+     }
     }
   
+    const [heading, Sethead]=useState("hello check");
 
-  };
+    const changeHead=()=>{
+
+      Sethead=(`new Data ${Math.ceil(Math.random()*10)}`);
+    };
+
   
+  console.log("heading",heading);
+
   return(
         <div>
           <h1> Hello-I am Learning React.</h1> 
         
-        {/* <Buttton/>
-        <Click/>
-        <Checkbox/>
-        <Checkbox/><Checkbox/>
-        <Checker/>
-        <Summary/>
-        <My/>
-        <JsxExample2/>
-        <JsxExample/>
-        <MultiElement/>
+       {/* <Buttton/> */}
+       {/* <Summary/> */}
+         {/* <Click/> */}
+        {/* <Checkbox/> */}
+        {/* <Checkbox/><Checkbox/> */}
+        {/* <Checker/> */}
         
-       <Timer/>
-       <User/> */}
+        {/* <My/> */}
+        {/* <JsxExample2/> */}
+        {/* <JsxExample/> */}
+        {/* <MultiElement/> */}
+        
+       {/* <Timer/> */}
+       <User/> 
+<Myhook/>
+       <h1>Hey, I am React First</h1>
+      {/* <button onClick={updateHeader}>Submit</button> */}
 
-        <div className={`box-cont bg-${theme}`}>Hey-Toggle succes</div> 
-        <button onClick={toggleTheme}>Toggle-Theme</button>
-       <UseState/>  </div>
-          );
-}
-export default App;
+{/* <button onClick={changeHead}>Dilip</button> */}
+         <div className={`box-cont bg-${theme}`}>Hey-Toggle succes</div>  
+        <button onClick={toggleTheme}>Toggle-Theme</button> 
+        <h1>Hey I am React First</h1>
+      {/* <button onClick={header}>Submit</button> <div style={{ backgroundColor }}> */}
+        
+       
+{/* </div> */}
+    </div>
+  );
+   }
+  
+  export default App;
 
   
+  
 
+// const[header, setheader]=useState(`Hello I am useState`);
+
+// const setting=()=>{
+//   setheader=(`second part in running`);
+// }
+// console.log("setheader",setheader);
+  
+//     const [header, setHeader] = useState('Hello, I am useState');
+//   const [backgroundColor, setBackgroundColor] = useState('white');
+  
+//   const updateHeader = () => {
+//     setHeader('Second part is running');
+    
+//     switch (backgroundColor) {
+//       case 'white':
+//         setBackgroundColor('lightblue');
+//         break;
+//       case 'lightblue':
+//         setBackgroundColor('lightgreen');
+//         break;
+//       case 'lightgreen':
+//         setBackgroundColor('lightcoral');
+//         break;
+//       case 'lightcoral':
+//         setBackgroundColor('lightpink');
+//         break;
+//       case 'lightpink':
+//         setBackgroundColor('lightyellow');
+//         break;
+//       case 'lightyellow':
+//         setBackgroundColor('lightblue'); // Loop back to the first color
+//         break;
+//       default:
+//         setBackgroundColor('white');
+//         break;
+//     }
+  
+  
+  
+
+ 
---
+++ src/usestate2.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+// import "/usetate.css"
+import "./user.css"
+function Myhook(){
+
+
+
+ const [head,setHead]=      useState('I am Here');
+ const changed=()=>{
+    setHead(`I am changing ${Math.ceil(Math.random()*10)+5}`);
+    console.log("Head",head);
+ }
+
+
+
+//Another useState code
+
+const [head2,setHead2]=useState('I am another Pratice point');
+const changed2=()=>{
+   setHead2(`I am another Changed i your React JS code ${Math.ceil(Math.random()*10)+50}`);
+   console.log("head2",head2);
+}
+
+
+ return(
+    <div><h1>{head}</h1>
+        <button className="Myhook" onClick={changed}>Clicker</button>
+<h1>{head2}</h1>
+        <button onClick={changed2}>Clicker-2.0</button>
+    </div>
+ );
+}
+ export default Myhook;
+ 
